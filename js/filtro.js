6d344139ffdb77cd2b89189d2586dad296203a7f
Gabrielle Pignatari <gabriellepignatari@gmail.com>
2023-10-19 20:47:31 -0300

    tela espirito santo - adicionando pontos normais

--- js/filtro.js
+++ js/filtro.js
@@ -1,4 +1,5 @@
 var elementoListaDestaques = document.getElementById('listaDestaques');
+var elementoListaPontos = document.getElementById('listaPontos');
 
 var listaDeDestaques = ["../images/masp.jpg",
                       '../images/bondinho-rj.webp',
@@ -9,13 +10,33 @@ var listaDeDestaques = ["../images/masp.jpg",
 var listaDeNomesDestaques = ["MASP",
                             'Bondinho',
                             'Praia ES',
-                            'Ouro Preto'
-                            ];                   
+                            'Ouro Preto',
+                            'MASP 2'
+                            ];  
+                            
+var listaEnderecosDestaques = ["Av. Paulista",
+                               "Av. Pedro Álvares Cabral - SP",
+                               "R. da Cantareira - SP",
+                               "Pico de Montanha - Rj",
+                               "Av. Paulista"
+                            ];
+
 //adicionando os pontos do vetor na tela
 for (var i = 0; i < listaDeDestaques.length; ++i) {
     elementoListaDestaques.innerHTML += `
-    <figure style="margin-top: 5%;">
+    <figure style="margin-top: 2%;">
       <img src = ${listaDeDestaques[i]} title = ${listaDeNomesDestaques[i]}></img>
       <p class = legenda>${listaDeNomesDestaques[i]}</p>
+      <p class = legenda>${listaEnderecosDestaques[i]}</p>
+    </figure>`
+}
+
+//adicionando os pontos normais do vetor na tela (mesmos vetores só pra testar)
+for (var i = 0; i < listaDeDestaques.length; ++i) {
+    elementoListaPontos.innerHTML += `
+    <figure style="margin-top: 2%;">
+      <img src = ${listaDeDestaques[i]} title = ${listaDeNomesDestaques[i]}></img>
+      <p class = legenda>${listaDeNomesDestaques[i]}</p>
+      <p class = legenda>${listaEnderecosDestaques[i]}</p>
     </figure>`
 }
