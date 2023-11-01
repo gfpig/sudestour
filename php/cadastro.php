4a3ec1909b9b4f17b82f5b14f6fc6fe9e77aa53f
Gabrielle Pignatari <gabriellepignatari@gmail.com>
2023-11-01 17:04:04 -0300

    cadastra anunciante (enviar form) através do botão

--- php/cadastro.php
+++ php/cadastro.php
@@ -29,40 +29,62 @@
             <img src="../images/icones/ajuda.png" width="15px" title="O usuário anunciante poderá sugerir locais de interesse para serem adicionados ao site" style="margin-left:5px;"><br>
         </div>
 
-        <form action="cadastro.php" method="POST">
+        <form action="cadastro.php" method="POST" style="height:100%">
             <label>Nome:</label><br>
-            <input type="text" name="nome" class="texto">
+            <input type="text" name="nome" class="texto" required/>
             <label>CNPJ:</label><br>
-            <input type="text" name="cnpj" class="texto">
+            <input type="text" name="cnpj" class="texto" required/>
             <label>Senha:</label><br>
-            <input type="text" name="senha" class="texto">
+            <input type="text" name="senha" class="texto" required/>
             <label>E-mail:</label><br>
-            <input type="text" name="email" class="texto">
-            <button type="submit" class="cadastrar">CADASTRAR</button>
+            <input type="email" name="email" class="texto" required/>
+            <button type="submit" name="btnAnunciante" class="cadastrar">CADASTRAR</button>
 
             <!-- Código para cadastrar um usuário do tipo ANUNCIANTE -->
             <?php
-                if(empty($_POST["nome"])||empty($_POST["cnpj"])||empty($_POST["senha"])||empty($_POST["email"])){
-                    echo "<script type='text/javascript'>alert('Preencha todos os campos corretamente.');</script>";
-                    exit;       
-                } else {                       
-                    $nome = $_POST["nome"];                
-                    $cnpj = $_POST["cnpj"];
-                    $senha = $_POST["senha"];   
-                    $email = $_POST["email"];           
-                    $query = "INSERT INTO anunciante(`Cnpj`, `SenhaAnunciante`,`EmailAnunciante`, `NomeAnunciante`) VALUES ('$cnpj', '$senha', '$email', '$nome')";                    
-                if(mysqli_query($mysqli, $query)){ //se a query for bem sucedida, exiba um pop-up
-                    echo "<script>document.getElementByName('cnpj').value=(''); </script>";
-                    echo "<script>document.getElementByName('nome').value=(''); </script>";
-                    echo "<script>document.getElementByName('senha').value=(''); </script>";
-                    echo "<script>document.getElementByName('email').value=(''); </script>";
-                    echo "<script type='text/javascript'>alert('Seu cadastrado foi realizado com sucesso :)');</script>"; 
-                } else{ //se a query for mal sucedida, exiba outro pop-up
-                    echo "<script type='text/javascript'>alert('Ocorreu um erro e nenhum dado foi inserido.');</script>";  
-                    }                 
-                }               
-                mysqli_close($mysqli);              
-            ?>    
+            //echo "<script>
+           //     var form = document.getElementById('myForm');
+           //         function handleForm(event) { event.preventDefault(); } 
+            //        form.addEventListener('submit', handleForm);
+           //     </script>";
+                //echo "<script type='text/javascript'>alert('O php foi ativado');</script>";
+                //require_once 'cadastro.php';
+                if (isset($_POST['btnAnunciante'])) { //EM TEORIA ativa a função apenas quando o botão for clicado
+                    echo "<script type='text/javascript'>alert('O botão foi apertado!');</script>";
+                   /* if(empty($_POST["nome"])||empty($_POST["cnpj"])||empty($_POST["senha"])||empty($_POST["email"])){
+                        echo "<script type='text/javascript'>alert('Preencha todos os campos corretamente.');</script>";
+                        return;      
+                    } ^ ELA VERIFICA OS SÓ DE ENTRAR NA PÁGINA SUCUMBA */ 
+                    //else {
+                    //function cadastrarAnunciante() {   
+                        $mysqli = new mysqli("localhost", "root", "", "sudestour");                    
+                        $nome = $_POST["nome"];                
+                        $cnpj = $_POST["cnpj"];
+                        $senha = $_POST["senha"];   
+                        $email = $_POST["email"];           
+                        $query = "INSERT INTO anunciante(`Cnpj`, `SenhaAnunciante`,`EmailAnunciante`, `NomeAnunciante`) VALUES ('$cnpj', '$senha', '$email', '$nome')";    
+                                        
+                        if(mysqli_query($mysqli, $query)){ //se a query for bem sucedida, exiba um pop-up
+                            echo "<script>document.getElementByName('cnpj').value=(''); </script>";
+                            echo "<script>document.getElementByName('nome').value=(''); </script>";
+                            echo "<script>document.getElementByName('senha').value=(''); </script>";
+                            echo "<script>document.getElementByName('email').value=(''); </script>";
+                            echo "<script type='text/javascript'>alert('Seu cadastrado foi realizado com sucesso :)');</script>";
+                            header("location: {$_SERVER['PHP_SELF']}");
+                            //exit; 
+                        } 
+                        else{ //se a query for mal sucedida, exiba outro pop-up
+                            echo "<script type='text/javascript'>alert('Ocorreu um erro e nenhum dado foi inserido.');</script>";  
+                        }                 
+                   // }               
+                        mysqli_close($mysqli);  
+                    //}
+
+                    /*if(array_key_exists('submit',$_POST)){
+                        cadastrarAnunciante();
+                     }*/
+                } 
+            ?>
         </form>
     </div>
     <div class="cadastro_turista">
@@ -80,4 +102,5 @@
         <input type="text" class="texto"><br>
         <button class="cadastrar">CADASTRAR</button>
     </div>
+    
 </body>
