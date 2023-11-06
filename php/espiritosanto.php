fe859271f240b3637a6cfd50057769cf08528941
Gabrielle Pignatari <gabriellepignatari@gmail.com>
2023-11-05 21:22:41 -0300

    adicionando tela perfil e pagamento

--- php/espiritosanto.php
+++ php/espiritosanto.php
@@ -9,6 +9,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Pontos Turisticos do Espírito Santo</title>
+    <link rel="shortcut icon" 
+          href="../images/logos/sudestour_logo.png">
     <link rel="stylesheet" href="../css/style_filtro.css">  
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
