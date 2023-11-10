65744c5deede50ef8062fade564ed8ec8ac3169b
Gabrielle Pignatari <gabriellepignatari@gmail.com>
2023-11-09 21:33:43 -0300

    só salvando

--- php/aplicar_filtro.php
+++ php/aplicar_filtro.php
@@ -1,6 +1,12 @@
 <?php
 include("conecta.php");
 
+$bairro = mysqli_real_escape_string($mysqli, $_POST['bairros']);
+$cidade = mysqli_real_escape_string($mysqli, $_POST['cidades']);
+
+$result = $mysqli->execute("SELECT * FROM local WHERE Bairro = '{$bairro}' AND Cidade = '{$cidade}'  ORDER BY `Bairro` ASC");
+
+
 /*$cidade = $_GET['cidadeSelecionada'];
 
 $result = $mysqli->prepare("SELECT DISTINCT `Bairro` FROM `local` WHERE IdCategoria =':cidadeSelecionada'  ORDER BY `Bairro` ASC");
@@ -12,7 +18,7 @@ while($row = $result->fetch_assoc()) {
     echo '<option value ="' . $row['Bairro'] . '">' . $row['Bairro'] . '</option>';
 }*/
 
-$categoria = $_GET['categoria'];
+/*$categoria = $_GET['categoria'];
 
 $query = $mysqli -> prepare("SELECT DISTINCT Bairro FROM local WHERE IdCategoria =':categoria'  ORDER BY `Bairro` ASC");
 
@@ -24,5 +30,5 @@ foreach ($registros as $option) {
 ?>
     <option value ="<?php echo $option['Bairro']?>"><?php echo $option['Bairro']?></option>
 <?php
-}
+}*/
 ?>
